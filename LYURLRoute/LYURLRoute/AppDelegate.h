c032d91e1a6a8386623c431bccb83499fbb02293
赵学良 <zxl12896@ly.com>
2017-04-17 22:35:12 +0800

    创建项目

---
+++ LYURLRoute/LYURLRoute/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LYURLRoute
+//
+//  Created by 赵学良 on 17/4/17.
+//  Copyright © 2017年 LYKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LYURLRoute/LYURLRoute/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LYURLRoute
+//
+//  Created by 赵学良 on 17/4/17.
+//  Copyright © 2017年 LYKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
